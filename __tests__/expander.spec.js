63e3a6a72b9fdad3098f001959dc2086d98ea214
FallenMax <fallenmax@gmail.com>
2016-10-25 00:15:59 +0800

    support expand when cursor is inside abbreviation

--- __tests__/expander.spec.js
+++ __tests__/expander.spec.js
@@ -3,48 +3,126 @@
 
 const trim = str => str.replace(/ +\n/g, '\n')
 
-
 it('extract abbreviation', () => {
   const { extract } = require('../lib/expander')
-  const input = 'aaa bbb  #header.test'
-  const output = extract(input)
+  const input = `aaa bbb  #header.test☯` // use ☯ as cursor pos
+  const output = extract(input.replace('☯', ''), input.indexOf('☯'))
 
-  expect(output).toBe(
-    `#header.test`
-  )
+  expect(output).toEqual({
+    abbr: `#header.test`,
+    abbrStart: 9,
+    abbrEnd: 21,
+  })
 })
 
 it('extract abbreviation: with []/{} behind', () => {
   const { extract } = require('../lib/expander')
-  const input = 'a random[] and{} .real-dom>p{A paragraph}+.row>.col*5'
-  const output = extract(input)
+  const input = 'a random[] and{} .real-dom>p{A paragraph}+.row>.col*5☯'
+  const output = extract(input.replace('☯', ''), input.indexOf('☯'))
 
-  expect(output).toBe(
-    `.real-dom>p{A paragraph}+.row>.col*5`
-  )
+  expect(output).toEqual({
+    abbr: `.real-dom>p{A paragraph}+.row>.col*5`,
+    abbrStart: 17,
+    abbrEnd: 53,
+  })
+})
+
+it('extract abbreviation: with []/{} behind and cursor inside', () => {
+  const { extract } = require('../lib/expander')
+  const input = 'a random[] and{} .rea☯l-dom>p{A paragraph}+.row>.col*5'
+  const output = extract(input.replace('☯', ''), input.indexOf('☯'))
+
+  expect(output).toEqual({
+    abbr: `.real-dom>p{A paragraph}+.row>.col*5`,
+    abbrStart: 17,
+    abbrEnd: 53,
+  })
 })
 
 it('extract abbreviation: with unpaired []/{} behind', () => {
   const { extract } = require('../lib/expander')
-  const input = 'a random[ and{ .fake-dom>.row>.col*5 .real-dom>p{A paragraph}+.row>.col*5'
-  const output = extract(input)
+  const input = 'a random[ and{ .fake-dom>.row>.col*5 .real-dom>p{A paragraph}+.row>.col*5☯'
+  const output = extract(input.replace('☯', ''), input.indexOf('☯'))
 
-  expect(output).toBe(
-    `.real-dom>p{A paragraph}+.row>.col*5`
-  )
+  expect(output).toEqual({
+    abbr: `.real-dom>p{A paragraph}+.row>.col*5`,
+    abbrStart: 37,
+    abbrEnd: 73,
+  })
 })
 
 it('extract abbreviation: only take last abbr', () => {
   const { extract } = require('../lib/expander')
-  const input = 'a random[] and{} .fake-dom>.row>.col*5 .real-dom>p{A paragraph}+.row>.col*5'
-  const output = extract(input)
+  const input = 'a random[] and{} .fake-dom>.row>.col*5 .real-dom>p{A paragraph}+.row>.col*5☯'
+  const output = extract(input.replace('☯', ''), input.indexOf('☯'))
 
-  expect(output).toBe(
-    `.real-dom>p{A paragraph}+.row>.col*5`
-  )
+  expect(output).toEqual({
+    abbr: `.real-dom>p{A paragraph}+.row>.col*5`,
+    abbrStart: 39,
+    abbrEnd: 75,
+  })
+})
+
+it('extract abbreviation: cursor inside abbr (1)', () => {
+  const { extract } = require('../lib/expander')
+  const input = 'a random[] and{} .☯fake-dom>.row>.col*5 .real-dom>p{A paragraph}+.row>.col*5 somerandom'
+  const output = extract(input.replace('☯', ''), input.indexOf('☯'))
+
+  expect(output).toEqual({
+    abbr: `.fake-dom>.row>.col*5`,
+    abbrStart: 17,
+    abbrEnd: 38,
+  })
+})
+
+it('extract abbreviation: cursor inside abbr (2)', () => {
+  const { extract } = require('../lib/expander')
+  const input = 'a random[] and{} .fake-dom>.row>☯.col*5 .real-dom>p{A paragraph}+.row>.col*5 somerandom ]'
+  const output = extract(input.replace('☯', ''), input.indexOf('☯'))
+
+  expect(output).toEqual({
+    abbr: `.fake-dom>.row>.col*5`,
+    abbrStart: 17,
+    abbrEnd: 38,
+  })
+})
+
+it('extract abbreviation: cursor inside abbr (3)', () => {
+  const { extract } = require('../lib/expander')
+  const input = 'a random[] and{} .fake-dom>.row>.col*5 .real-dom>p{☯A paragraph}+.row>.col*5 somerandom }'
+  const output = extract(input.replace('☯', ''), input.indexOf('☯'))
+
+  expect(output).toEqual({
+    abbr: `.real-dom>p{A paragraph}+.row>.col*5`,
+    abbrStart: 39,
+    abbrEnd: 75,
+  })
 })
 
 
+it('extract abbreviation: cursor inside abbr (4)', () => {
+  const { extract } = require('../lib/expander')
+  const input = 'a random[] and{} .fake-dom>.row>.col*5 .real-dom>p{A paragraph☯}+.row>.col*5 somerandom }'
+  const output = extract(input.replace('☯', ''), input.indexOf('☯'))
+
+  expect(output).toEqual({
+    abbr: `.real-dom>p{A paragraph}+.row>.col*5`,
+    abbrStart: 39,
+    abbrEnd: 75,
+  })
+})
+
+it('extract abbreviation: cursor inside abbr (5)', () => {
+  const { extract } = require('../lib/expander')
+  const input = 'a random[] and{} .fake-dom>.row>.col*5 .real-dom>p{A paragraph}+.row☯>.col*5 somerandom }'
+  const output = extract(input.replace('☯', ''), input.indexOf('☯'))
+
+  expect(output).toEqual({
+    abbr: `.real-dom>p{A paragraph}+.row>.col*5`,
+    abbrStart: 39,
+    abbrEnd: 75,
+  })
+})
 
 it('expand abbreviation: simple', () => {
   const { expand } = require('../lib/expander')
--- extension.js
+++ extension.js
@@ -10,42 +10,39 @@ function activate(context) {
     if (!editor) {
       return
     }
-    // if (editor.document.languageId !== 'javascript') {
-    //   return
-    // }
+
 
     const document = editor.document
     const selection = editor.selection
     const { line, character } = selection.active // current cursor position
     const curLine = document.lineAt(line).text
 
-    const abbr = extract(curLine.substr(0, character))
-
     const { tabSize, insertSpaces } = editor.options
     const tab = insertSpaces ? ' '.repeat(tabSize) : '\t'
     const tabsAtStart = new RegExp(`^(${tab})*`)
     const indentLevel = (tabsAtStart.exec(curLine) || [])[0].length / tab.length
 
-    let output
-    if (abbr) {
-      try {
-        output = expand(abbr, { tab, indentLevel })
-        editor.edit(edit => {
+
+    let { abbr, abbrStart, abbrEnd } = extract(curLine, character)
+    try {
+      if (abbr) {
+        const output = expand(abbr, { tab, indentLevel })
+        return editor.edit(edit => {
             // edit.replace doesn't work well here, it messes up cursor position/selection
-            edit.delete(new vscode.Range(line, character - abbr.length, line, character))
-            edit.insert(new vscode.Position(line, character - abbr.length), output)
+            edit.delete(new vscode.Range(line, abbrStart, line, abbrEnd))
+            edit.insert(new vscode.Position(line, abbrStart), output)
           })
           .then(() => {
             const cursor = selection.active // current cursor position after edit
-            editor.revealRange(new vscode.Range(line, character - abbr.length, cursor.line, cursor.character))
+            editor.revealRange(new vscode.Range(line, abbrStart, cursor.line, cursor.character))
           })
-      } catch (e) {
-        console.error('[mithril-emmet]', e)
-        console.error({ abbr })
-        vscode.window.showErrorMessage('[mithril-emmet] Failed to expand: ' + abbr)
+      } else {
+        return vscode.window.showInformationMessage('[mithril-emmet] Nothing to expand')
       }
-    } else {
-      vscode.window.showInformationMessage('[mithril-emmet] Nothing to expand')
+    } catch (e) {
+      console.error('[mithril-emmet]', e)
+      console.error({ abbr })
+      vscode.window.showErrorMessage('[mithril-emmet] Failed to expand: ' + abbr)
     }
   })
 
